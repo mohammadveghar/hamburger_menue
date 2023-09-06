a1c96575c40b9deab6edd1c8b16c31c4299137af
Mohammad Vaghar <mohammad.veghar.92@gmail.com>
2023-09-06 21:50:57 +0330

    I created Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from 'styled-components'
+
+
+//............................................
+
+const Ul = styled.ul`
+list-style : none ;
+display : flex ;
+justify-content :right ;
+margin : 20px  50px ;
+z-index : 15 ;
+li {
+   padding : 18px 30px;
+} 
+@media(max-width :768px) {
+   margin : 0;
+   flex-direction : column;
+   background : #1565df ;
+   position : fixed;
+   top : 0 ;
+   right : 0 ;
+   height : 100vh;
+   width : 300px;
+   padding-top : 3.5rem;
+   li {
+   color : #fff;
+}
+}
+`
+
+const Navbar = () => {
+   return (
+      <Ul>
+         <li>Home</li>
+         <li>Docs</li>
+         <li>Buy me a coffe</li>
+         <li>Github</li>
+     </Ul>
+   )
+}
+export default Navbar;
